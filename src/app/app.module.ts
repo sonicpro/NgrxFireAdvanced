a4a109f04b41f91e20571adb05db888313914904
Volodymyr Tsurko <chegnenado@yahoo.com>
2021-08-22 20:29:32 +0300

    Added logger metareducer and Store-devtools.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
+import { Action, ActionReducer, StoreModule, MetaReducer } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
+import { AppState } from './models/app-state.model';
 import { postReducer } from './reducers/post.reducer';
+
+const logger: (reducer: ActionReducer<any, Action>) => any = reducer => {
+	return function newReducer(state: AppState, action: Action) {
+		console.groupCollapsed(action.type);
+		const nextState = reducer(state, action);
+		console.log(`%c prev state`, `color: #9E9E9E; font-weight: bold`, state);
+		console.log(`%c action`, `color: #03A9F4; font-weight: bold`, action);
+		console.log(`%c next state`, `color: #4CAF50; font-weight: bold`, nextState);
+		console.groupEnd();
+		return nextState;
+	};
+}
+
+const metaReducers: MetaReducer<any>[] = [logger];
 
 @NgModule({
   declarations: [
@@ -13,6 +29,11 @@ import { postReducer } from './reducers/post.reducer';
     BrowserModule,
     StoreModule.forRoot({
       post: postReducer
+    }, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10
     })
   ],
   providers: [],
